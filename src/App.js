43acc501e2c3799d8e28d9f25340dc1da252390e
Jeff Soriano <soriano.jeffm@gmail.com>
2021-01-27 13:59:43 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
+import React, { useEffect, useState } from 'react';
+import lottery from './lottery';
 
 function App() {
+  const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
+  const [value, setValue] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [winMessage, setWinMessage] = useState('');
+
+  useEffect(() => {
+    async function loadWeb3() {
+      const manager = await lottery.methods.manager().call();
+
+      setManager(manager);
+      update();
+    }
+
+    loadWeb3();
+  }, []);
+
+  async function update() {
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    setPlayers(players);
+    setBalance(balance);
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    setLoading(true);
+    await lottery.methods.enter().send({ from: accounts[0], value: web3.utils.toWei(value, 'ether') });
+    update();
+    setLoading(false);
+  }
+
+  async function onClick() {
+    const accounts = await web3.eth.getAccounts();
+
+    setLoading(true);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    update();
+    setWinMessage('A winner has been picked!');
+    setLoading(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Lottery Contract</h2>
+      <p>This contract is managed by {manager}.
+        There are currently {players.length} people entered,
+        competing to win {web3.utils.fromWei(balance, 'ether')} ether!
+      </p>
+      <hr />
+      {loading ? <p>Waiting for transaction to complete...</p> :
+        <div>
+          <form onSubmit={onSubmit}>
+            <h4>Want to try your luck?</h4>
+            <div>
+              <label>Amount of ether to enter</label>
+              <input
+                value={value}
+                onChange={event => setValue(event.target.value)}>
+              </input>
+              <button type="submit">Enter</button>
+            </div>
+          </form>
+
+          <hr />
+
+          <h4>Ready to pick a winner?</h4>
+          {winMessage && <h5>{winMessage}</h5>}
+          <button onClick={onClick}>Pick a winner!</button>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/lottery.js
@@ -0,0 +1,48 @@
+import web3 from './web3';
+
+const address = '0x89a7a5d894797a6f26ACA2E5f2F3b39f29bB9f96';
+
+const abi = [
+    {
+        inputs: [], stateMutability: 'nonpayable', type: 'constructor'
+    },
+    {
+        inputs: [],
+        name: 'enter',
+        outputs: [],
+        stateMutability: 'payable',
+        type: 'function'
+    },
+    {
+        inputs: [],
+        name: 'getPlayers',
+        outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        inputs: [],
+        name: 'manager',
+        outputs: [{ internalType: 'address', name: '', type: 'address' }],
+        stateMutability: 'view',
+        type: 'function'
+    },
+    {
+        inputs: [],
+        name: 'pickWinner',
+        outputs: [],
+        stateMutability: 'payable',
+        type: 'function'
+    },
+    {
+        inputs: [{
+            internalType: 'uint256', name: '', type: 'uint256'
+        }],
+        name: 'players',
+        outputs: [{ internalType: 'address', name: '', type: 'address' }],
+        stateMutability: 'view',
+        type: 'function'
+    }
+];
+
+export default new web3.eth.Contract(abi, address);
